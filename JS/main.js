6bd80348a33837c8008cb36ebaa61aaac0ddaf1a
57naveen <naveenkishore65@gmail.com>
2024-01-02 18:05:32 +0530

    hgfde

--- JS/main.js
+++ JS/main.js
@@ -89,7 +89,105 @@ console.log(Number(false));
 
 //------------------------Number Method------------------------------//
 
-console.log(Number.isInteger(21));
+const number1="232"
+
+//this function is used to check the value is integer or not 
+console.log(Number.isInteger(21)); 
+
+
+//This function is used to to give only float value from the variable
+const myFloat2="23.324324efde"
+console.log(Number.parseFloat(myFloat2)); 
+
+//This function is used to give only int value from the variable
+console.log(Number.parseInt(myFloat2)); 
+
+//toFixed() formats a number to a given length after the decimal point
+console.log(Number.parseFloat(myFloat2).toFixed(3)); 
+
+//toString is used to change the any value into string 
+//output color should chanage into white
+console.log(myFloat2.toString()); 
+
+
+//The isNaN() method returns true if the value is NaN and of type Number. Otherwise, it returns false.
+console.log(Number.isNaN(NaN));
+
+console.log(isNaN('dsnj'));
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+//------------------------MAth Properties------------------------------//
+
+
+console.log(Math.PI);
+
+//truncate methed used to remove the decimal value after the deciaml value 
+console.log(Math.trunc(Math.PI));
+
+//round method used to round of the given value 
+//Ex : if value =3.5 the output :4
+//Ex : if value =3.1 the output :3
+console.log(Math.round(3.5));
+
+//ceil method will always round of the value if it is less then 5 after decimal point 
+console.log(Math.ceil(4.1));
+
+//The Math.floor() method rounds a number DOWN to the nearest integer
+//floor method will not round the value  the value if it is greater then 5 after decimal point 
+console.log(Math.floor(4.9));
+
+
+//pow() method returns the value of x to the power of y (xy)
+console.log(Math.pow(10,3));
+
+//min used to give the minimum value output
+console.log(Math.min(10,3,6,23,5));
+
+//max used to give the Maximum value output
+console.log(Math.max(10,3,6,23,5));
+``
+//This method is used to generate the random number.
+console.log(Math.random());
+
+
+// Math.random() * 10: Multiplies the random decimal by 10, giving a random decimal between 0 (inclusive) and 10 (exclusive).
+// Math.floor(Math.random() * 10): Rounds down the random decimal to the nearest whole number, resulting in an integer between 0 and 9.
+// Math.floor(Math.random() * 10) + 1: Adds 1 to the result, shifting the range to be between 1 and 10 (inclusive).
+
+console.log(Math.floor(Math.random()*10)+1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
